d3ea8754c800445d0b8e44f24511f1cbc9f2b2a8
jsexp <compromissed3@proton.me>
2024-02-08 16:31:34 +0100

    Se ha sacado el sistema de licencias.
    Se ha eliminado el sistema de licencias en el start-up ya que el plugin es open source y necesariamente necesitas tener una key para poder utilizarlo correctamente.

--- bukkit/src/main/java/today/netshield/bukkit/NetShield.java
+++ bukkit/src/main/java/today/netshield/bukkit/NetShield.java
@@ -1,15 +1,8 @@
 package today.netshield.bukkit;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import lombok.Getter;
-import okhttp3.*;
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 import today.netshield.bukkit.listener.PlayerListener;
-import today.netshield.bukkit.utils.CC;
-
-import java.io.IOException;
 
 public final class NetShield extends JavaPlugin {
     @Getter
@@ -20,51 +13,7 @@ public final class NetShield extends JavaPlugin {
         saveDefaultConfig();
         instance = this;
 
-        CC.log("&7&m" + StringUtils.repeat("-", 24));
-        CC.log("&b&lNet&9&lShield");
-        CC.log("&f");
-        CC.log("&9Version: "+ getDescription().getVersion());
-        CC.log("&7&oChecking key...");
-
-        OkHttpClient client = new OkHttpClient();
-
-        Gson gson = new Gson();
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("key", getConfig().getString("KEY"));
-        String jsonString = gson.toJson(jsonObject);
-
-        RequestBody requestBody = RequestBody.create(jsonString, MediaType.parse("application/json"));
-
-        Request request = new Request.Builder()
-                .url("https://netshield.bombardeen.me/api/checkkey")
-                .post(requestBody)
-                .addHeader("Content-Type", "application/json")
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful()) {
-                String jsonResponse = response.body().string();
-                JsonObject responseObject = gson.fromJson(jsonResponse, JsonObject.class);
-                String status = responseObject.has("status") ? responseObject.get("status").getAsString() : null;
-
-                if (!status.equals("VALID_KEY")) {
-                    CC.log("&9Status: &c" + status);
-                    CC.log("&c&oShutting down the plugin...");
-                    getPluginLoader().disablePlugin(this);
-                } else {
-                    CC.log("&9Status: &a" + status);
-                    CC.log("&eThanks for using &lNetShield&e!");
-                    getServer().getPluginManager().registerEvents(new PlayerListener(), this);
-                }
-            } else {
-                CC.log("Error: " + response.code() + " - " + response.message());
-                getPluginLoader().disablePlugin(this);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        CC.log("&7&m" + StringUtils.repeat("-", 24));
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
     }
 
     @Override
--- bungeecord/src/main/java/today/netshield/bungee/NetShield.java
+++ bungeecord/src/main/java/today/netshield/bungee/NetShield.java
@@ -1,15 +1,11 @@
 package today.netshield.bungee;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import lombok.Getter;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
-import okhttp3.*;
 import today.netshield.bungee.listeners.PlayerListener;
-import today.netshield.bungee.utils.CC;
 import today.netshield.bungee.utils.ConfigFiles;
 
 import java.io.File;
@@ -37,52 +33,6 @@ public final class NetShield extends Plugin {
             throw new RuntimeException(e);
         }
 
-        CC.log("&7&m------------------------");
-        CC.log("&b&lNet&9&lShield");
-        CC.log("&f");
-        CC.log("&9Version: "+ getDescription().getVersion());
-        CC.log("&7&oChecking key...");
-
-        OkHttpClient client = new OkHttpClient();
-
-        Gson gson = new Gson();
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("key", getConfig().getString("KEY"));
-        String jsonString = gson.toJson(jsonObject);
-
-        RequestBody requestBody = RequestBody.create(jsonString, MediaType.parse("application/json"));
-
-        Request request = new Request.Builder()
-                .url("https://netshield.bombardeen.me/api/checkkey")
-                .post(requestBody)
-                .addHeader("Content-Type", "application/json")
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful()) {
-                String jsonResponse = response.body().string();
-                JsonObject responseObject = gson.fromJson(jsonResponse, JsonObject.class);
-                String status = responseObject.has("status") ? responseObject.get("status").getAsString() : null;
-
-                if (!status.equals("VALID_KEY")) {
-                    CC.log("&9Status: &c" + status);
-                    CC.log("&c&oShutting down the plugin...");
-                    getProxy().getPluginManager().unregisterCommands(this);
-                    getProxy().getPluginManager().unregisterListeners(this);
-                } else {
-                    CC.log("&9Status: &a" + status);
-                    CC.log("&eThanks for using &lNetShield&e!");
-                    getProxy().getPluginManager().registerListener(this, new PlayerListener());
-                }
-            } else {
-                CC.log("Error: " + response.code() + " - " + response.message());
-                getProxy().getPluginManager().unregisterCommands(this);
-                getProxy().getPluginManager().unregisterListeners(this);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        CC.log("&7&m------------------------");
+        getProxy().getPluginManager().registerListener(this, new PlayerListener());
     }
 }
